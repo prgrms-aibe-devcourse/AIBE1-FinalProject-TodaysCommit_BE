f3f6658332cb9e1cec2c52b5f75da695737771a2
soowan <nick6767676767@gmail.com>
2025-06-16 18:17:59 +0900

    init: 도메인 클래스 파일 추가 생성

---
+++ src/main/java/com/team5/catdogeats/notifications/domian/notifications.java
@@ -0,0 +1,6 @@
+package com.team5.catdogeats.notifications.domian;
+
+import com.team5.catdogeats.baseEntity.BaseEntity;
+
+public class notifications extends BaseEntity {
+}
---
+++ src/main/java/com/team5/catdogeats/products/domain/mapping/InventoryAdjustments.java
@@ -0,0 +1,6 @@
+package com.team5.catdogeats.products.domain.mapping;
+
+import com.team5.catdogeats.baseEntity.BaseEntity;
+
+public class InventoryAdjustments extends BaseEntity {
+}
