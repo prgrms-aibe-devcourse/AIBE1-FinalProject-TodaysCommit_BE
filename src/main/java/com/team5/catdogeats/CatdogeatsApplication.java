95540754202a7017a5da117bc889a60ac215dfe4
soowan <nick6767676767@gmail.com>
2025-06-16 11:58:13 +0900

    first commit

---
+++ src/main/java/com/team5/catdogeats/CatdogeatsApplication.java
@@ -0,0 +1,13 @@
+package com.team5.catdogeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatdogeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatdogeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team5/catdogeats/CatdogeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.catdogeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatdogeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
