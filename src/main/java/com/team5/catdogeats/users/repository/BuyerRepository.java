8f929800bf1c396ab933551fef44deb946fe4a8a
soowan <nick6767676767@gmail.com>
2025-06-20 01:31:16 +0900

    feat: provider과 providerId로 Buyers, Sellers 객체 조회

--- src/main/java/com/team5/catdogeats/users/repository/BuyerRepository.java
+++ src/main/java/com/team5/catdogeats/users/repository/BuyerRepository.java
@@ -1,10 +1,28 @@
 package com.team5.catdogeats.users.repository;
 
+import com.team5.catdogeats.users.domain.dto.BuyerDTO;
 import com.team5.catdogeats.users.domain.mapping.Buyers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BuyerRepository extends JpaRepository<Buyers, UUID> {
 
+    @Query("""
+      SELECT new com.team5.catdogeats.users.domain.dto.BuyerDTO(
+        b.user.id,
+        b.nameMaskingStatus
+      )
+      FROM Buyers b
+      JOIN b.user u
+      WHERE u.provider = :provider
+        AND u.providerId = :providerId
+    """)
+    Optional<BuyerDTO> findOnlyBuyerByProviderAndProviderId(
+            @Param("provider") String provider,
+            @Param("providerId") String providerId
+    );
 }
