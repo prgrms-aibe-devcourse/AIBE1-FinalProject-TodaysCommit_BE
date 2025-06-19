fa3623e6827df4be47f816f5e47b9ea37379fe7f
soowan <nick6767676767@gmail.com>
2025-06-19 11:51:36 +0900

    feat: 테스트 코드 service 로직 검증

--- src/main/java/com/team5/catdogeats/auth/redis/RefreshTokens.java
+++ src/main/java/com/team5/catdogeats/auth/redis/RefreshTokens.java
@@ -10,11 +10,10 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Getter
-@Builder
+@Builder(toBuilder = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24) // 1일 TTL
-
 //엔티티가 아니기 때문에 @Entity 어노테이션 생략
 public class RefreshTokens implements Serializable {
 
