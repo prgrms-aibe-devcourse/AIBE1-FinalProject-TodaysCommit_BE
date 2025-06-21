3eab448265b3476bcf33ed2f41eae40d9b27bfba
soowan <nick6767676767@gmail.com>
2025-06-21 19:27:42 +0900

    refactor: 탈퇴 로직 개선

--- src/main/java/com/team5/catdogeats/users/service/impl/WithdrawServiceImpl.java
+++ src/main/java/com/team5/catdogeats/users/service/impl/WithdrawServiceImpl.java
@@ -34,34 +34,43 @@ public class WithdrawServiceImpl implements WithdrawService {
         log.info("회원 탈퇴 시작");
         try {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            String firstAuthority = authentication.getAuthorities().stream()
-                    .findFirst()
-                    .map(GrantedAuthority::getAuthority)
-                    .orElseThrow(() -> new IllegalStateException("권한 정보가 없습니다."));
-
-            if (Objects.equals(firstAuthority, Role.ROLE_TEMP.toString())) {
-                throw new IllegalStateException("잘못된 권한을 가진 유저입니다.");
-            }
+            String firstAuthority = validate(authentication);
+            
             log.debug("firstAuthority: {}", firstAuthority);
             userMapper.softDeleteUserByProviderAndProviderId(userPrincipal.provider(),
                     userPrincipal.providerId(), Role.ROLE_WITHDRAWN.toString());
 
-            if (Objects.equals(firstAuthority, Role.ROLE_SELLER.toString())) {
-                log.info("seller 탈퇴 시작");
-                sellerMapper.softDeleteSellerByProviderAndProviderId(userPrincipal.provider(),
-                        userPrincipal.providerId(), OffsetDateTime.now(ZoneOffset.UTC));
-            }
+            trigger(userPrincipal, firstAuthority);
 
-            if (Objects.equals(firstAuthority, Role.ROLE_BUYER.toString())) {
-                log.info("buyer 탈퇴 시작");
-                buyerMapper.softDeleteBuyerByProviderAndProviderId(userPrincipal.provider(),
-                        userPrincipal.providerId(), OffsetDateTime.now(ZoneOffset.UTC));
-            }
         } catch (BadSqlGrammarException e) {
             log.error("sql 에러", e);
             throw e;
-        } catch (Exception e) {
-            throw new RuntimeException("무슨에러이?");
         }
+    }
+
+    private void trigger(UserPrincipal userPrincipal, String firstAuthority) {
+        if (Objects.equals(firstAuthority, Role.ROLE_SELLER.toString())) {
+            log.info("seller 탈퇴 시작");
+            sellerMapper.softDeleteSellerByProviderAndProviderId(userPrincipal.provider(),
+                    userPrincipal.providerId(), OffsetDateTime.now(ZoneOffset.UTC));
+        }
+
+        if (Objects.equals(firstAuthority, Role.ROLE_BUYER.toString())) {
+            log.info("buyer 탈퇴 시작");
+            buyerMapper.softDeleteBuyerByProviderAndProviderId(userPrincipal.provider(),
+                    userPrincipal.providerId(), OffsetDateTime.now(ZoneOffset.UTC));
+        }
+    }
+
+    private String validate(Authentication authentication) {
+        String firstAuthority = authentication.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElseThrow(() -> new IllegalStateException("권한 정보가 없습니다."));
+
+        if (Objects.equals(firstAuthority, Role.ROLE_TEMP.toString())) {
+            throw new IllegalStateException("잘못된 권한을 가진 유저입니다.");
+        }
+        return firstAuthority;
     }
 }
