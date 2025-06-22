a748e88946b5ca5e01eb4417dc6552fda3b6b975
soowan <nick6767676767@gmail.com>
2025-06-22 23:27:25 +0900

    refactor: string 으로 변경

--- src/main/java/com/team5/catdogeats/pets/repository/PetRepository.java
+++ src/main/java/com/team5/catdogeats/pets/repository/PetRepository.java
@@ -5,8 +5,13 @@ import com.team5.catdogeats.users.domain.mapping.Buyers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PetRepository extends JpaRepository<Pets, UUID> {
     List<Pets> findByBuyer(Buyers buyer);
+
+    Optional<Pets> findById(String id);
+
+    void deleteById(String id);
 }
