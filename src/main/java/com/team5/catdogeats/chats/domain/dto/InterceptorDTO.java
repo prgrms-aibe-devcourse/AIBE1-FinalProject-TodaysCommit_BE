d10610314156f0bc92923f7448fa5ccc9e5988d9
soowan <nick6767676767@gmail.com>
2025-06-27 03:05:39 +0900

    chore: yml 추가

--- src/main/java/com/team5/catdogeats/chats/domain/dto/InterceptorDTO.java
+++
@@ -1,9 +0,0 @@
-package com.team5.catdogeats.chats.domain.dto;
-
-import com.team5.catdogeats.auth.dto.UserPrincipal;
-
-public record InterceptorDTO(String provider,
-                             String providerId,
-                             String role,
-                             UserPrincipal userPrincipal) {
-}
