df0ec1b574ece3420c14033b27dd6ff816e05c96
YoungBeen <rhdudqls73@gmail.com>
2025-06-29 00:44:02 +0900

    "refactor : redis 설정"

--- src/main/java/com/team5/catdogeats/global/config/SecurityConfig.java
+++ src/main/java/com/team5/catdogeats/global/config/SecurityConfig.java
@@ -25,10 +25,12 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequest
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 @Slf4j
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -70,7 +72,7 @@ public class SecurityConfig {
                             .deleteCookies("JSESSIONID")
                             .permitAll())
                     .securityContext(securityContext ->
-                            securityContext.requireExplicitSave(false))                // 🔧 SecurityContext 자동 저장 활성화
+                            securityContext.requireExplicitSave(false))                // SecurityContext 자동 저장 활성화
                     .exceptionHandling(exceptions -> exceptions
                             .authenticationEntryPoint((request, response, authException) -> {
                                 // 인증되지 않은 사용자를 로그인 페이지로 리다이렉트
