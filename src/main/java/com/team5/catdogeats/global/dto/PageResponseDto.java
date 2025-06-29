6025d9b2ac15126fc7c809bdc0a951f7d2172cc2
KimS1Yong <sdragon0928@naver.com>
2025-06-29 17:50:58 +0900

    refact: PageResponseDto에 형변환 메서드 추가하여 그 형태에 맞게 수정

--- src/main/java/com/team5/catdogeats/global/dto/PageResponseDto.java
+++ src/main/java/com/team5/catdogeats/global/dto/PageResponseDto.java
@@ -1,5 +1,7 @@
 package com.team5.catdogeats.global.dto;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 public record PageResponseDto<T>(
@@ -10,4 +12,14 @@ public record PageResponseDto<T>(
         int totalPages,
         boolean last // 현재 페이지가 마지막 페이지인가?
 ) {
+    public static <T> PageResponseDto<T> from(Page<T> page) {
+        return new PageResponseDto<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isLast()
+        );
+    }
 }
